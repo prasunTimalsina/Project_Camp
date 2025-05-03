4157462edfd5324a6ca3bc0b767ec790da1ebff0
Prasun Timalsina <timalsinaprasun61@gmail.com>
2025-05-03 16:03:00 +0545

    Finish boilerplate code and Add constant file and mail utility

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+
 const app = express();
 
-app.use(express.urlencoded());
-app.use(express.json());
+//router imports
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import app from "./app";
+import app from "./app.js";
 import dotenv from "dotenv";
-import connectDb from "./db/db";
+import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./.env" });
-
-connectDb()
-  .then()
-  .catch((err) => {
-    console.log("Error connection database", err);
-    process.exit(1);
-  });
+dotenv.config({
+  path: "./.env",
+});
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Mongodb connection error", err);
+    process.exit(1);
+  });
